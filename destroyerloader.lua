3a2ff2916d19f91d320eb88c94f96f5a560901a3
blueman5 <147107803+blueman5@users.noreply.github.com>
2024-06-01 19:01:02 -0300

    Create destroyerloader.lua

---
+++ destroyerloader.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/blueman5/ESS/main/UI.lua'))()
+wait(2)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/blueman5/ESS/main/Destroyer.lua'))()
